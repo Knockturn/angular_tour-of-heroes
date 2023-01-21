e205edfa7e8c2a337af3f64074d065668a2b0455
Phi <pkbrauner@gmail.com>
2023-01-21 08:23:19 +0100

    Add routing, create dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { HeroesComponent } from './components/heroes/heroes.component';
 import { FormsModule } from '@angular/forms';
 import { HeroDetailComponent } from './components/hero-detail/hero-detail.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { AppRoutingModule } from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent],
-  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [// no need to place any providers due to the `providedIn` flag...
      ],
   bootstrap: [AppComponent],
